98ccdb9ca061a936c68de907baf36337d0b0ea79
Rasmus Rask <rasse.rask@gmail.com>
2022-11-30 16:51:11 +0200

    Split now has an initialised string array with a while loop

--- ft_split.c
+++ ft_split.c
@@ -1,22 +1,51 @@
 #include "libft.h"
 
+static int wordcount(char const *s, char c) //word count
+{
+	int		count;
+	int		i;
+	i = 0;
+	count = 1;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			count++;
+		}
+		i++;
+	}
+	return (count);
+}
+
 char **ft_split(char const *s, char c)
 {
 	int i;
 	int count;
-	int len;
-	char *temp[50];
+	// int len;
+	char *temp;
 	char **strarr;
 
 	if (!s || !*s)
 		return (NULL);
 	if (!c)
-		return ((char *)s);
+		return ((char **)s);
 	i = 0;
-	count = 0;
-	//A function that counts the occurence of c. 
+	count = wordcount(s, c); //Count represents the number of words/strings we need to allocate for.
+	strarr = (char **)malloc(sizeof(char) + (count + 1));
+	while (s[i])
+	{
+		if (s[i] == c)
+			return(0);
+		while (count > 0)
+		{
+			
+		}
+		i++;
+	}
+	//A function that counts the occurence of c, ie. words/strings. 
 	// Use that count to malloc a **strarr.
 	// so on
+	return (strarr);
 }
 
 int main(void)
@@ -28,7 +57,7 @@ int main(void)
 	str = "Split at space";
 	c = ' ';
 	res = ft_split(str, c);
-	printf("%s\n", res);
+	printf("%s\n", *res);
 	return (0);
 }
 
