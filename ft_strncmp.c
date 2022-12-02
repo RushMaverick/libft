7aa0698d3a2f0ed41768b61bdcd35331ba4d9c88
Rasmus Rask <rasse.rask@gmail.com>
2022-12-02 11:44:02 +0200

    Fixed errors with strncmp, if string1 is contained within string2 we got an overflow

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: rrask <rrask@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 15:08:06 by rrask             #+#    #+#             */
-/*   Updated: 2022/11/22 11:34:44 by rrask            ###   ########.fr       */
+/*   Updated: 2022/12/02 11:41:25 by rrask            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,16 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while ((unsigned char)s1[i] == (unsigned char)s2[i])
+	if (n == 0)
+		return (0);
+	while ((unsigned char)s1[i] == (unsigned char)s2[i] && n - 1 > i)
 	{
-		if (n == 0)
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		if ((unsigned char)s1[i] == '\0' && (unsigned char)s2[i] == '\0')
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+			return (0);
 		i++;
-		n--;
 	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
