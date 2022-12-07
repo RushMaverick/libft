9295999b7854ccc72fb951f3988c955bdeeae5f2
Rasmus Rask <rasse.rask@gmail.com>
2022-12-07 16:50:59 +0200

    split in a good way, implementing a helper function to iterate through the jth and ith dimension separately from each other.

--- ft_split.c
+++ ft_split.c
@@ -1,9 +1,10 @@
 #include "libft.h"
+#include <stdio.h>
 
-static int wordcount(char const *s, char c) //word count
+static int	wordcount(char const *s, char c) //word count
 {
-	int		count;
-	int		i;
+	int count;
+	int i;
 	i = 0;
 	count = 1;
 	while (s[i] != '\0')
@@ -15,48 +16,59 @@ static int wordcount(char const *s, char c) //word count
 	return (count);
 }
 
-
-static char **applytoarr(char **strarr, char const *s, char c)
+char *writeuntilc(const char *s, int c)
 {
+	int i;
 	char *temp;
 
-	while (strarr)
+	i = 0;
+	temp = (char*)s;
+	while (*s != c && s[i])
 	{
-		// Move pointer from strarr using strarr vs int i. 
-		temp = ft_strdup(s);
-		free(temp);
-		strarr++;
+		s++;
+		i++;
 	}
-	return (strarr);
+	temp = ft_substr(temp, 0, ft_strlen(temp) - ft_strlen(s));
+	return (temp);
 }
 
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-	int count;
-	// int len;
-	char **strarr;
+	int		count;
+	int		j;
+	char	**strarr;
 
+	j = 0;
+	strarr = 0;
 	if (!s || !*s)
 		return (NULL);
 	if (!c)
 		return ((char **)s);
-	count = wordcount(s, c); //Count represents the number of words/strings we need to allocate for.
-	strarr = (char **)malloc(sizeof(char) + (count + 1));
-	strarr[count] = '\0';
-	applytoarr(strarr, s, c);
+	count = wordcount(s, c);
+		//Count represents the number of words/strings we need to allocate for.
+	strarr = (char **)malloc(sizeof(char *) * (count) + 1);
+	if (!strarr)
+		return (NULL);
+	while (j < count) // This could be done inside writeuntilc
+	{
+		// while (*s != c)
+		// 	s++;
+		strarr[j] = writeuntilc(s, c);
+		j++;
+	}
 	return (strarr);
 }
 
-int main(void)
+int	main(void)
 {
-	char *str;
-	char **res;
-	char c;
+	char	*str;
+	char	**res;
+	char	c;
 
 	str = "Split at space";
 	c = ' ';
 	res = ft_split(str, c);
-	printf("%s\n", *res);
+	printf("%s\n", res[0]);
 	return (0);
 }
 
